d39706bbfe503c4256e7e49104317f5c29658b3f
Vanessa Silva <vanessaaf_silva@hotmail.com>
2021-12-31 00:39:19 +0000

    adding codes to read empirical datasets

---
+++ read_empirical.R
@@ -0,0 +1,43 @@
+#############################################################
+################# READ EMPIRICAL DATASET ####################
+##############################################################
+
+
+## read TEST and TRAIN time series set files
+realts_TEST <- t(readARFF(paste0(dir, data_name, "/", data_name, "_TEST.arff")))
+realts_TRAIN <- t(readARFF(paste0(dir, data_name, "/", data_name, "_TRAIN.arff")))
+
+## get and store all classes
+classes_TEST <- realts_TEST[nrow(realts_TEST), ]
+realts_TEST <- realts_TEST[-nrow(realts_TEST), ]
+classes_TRAIN <- realts_TRAIN[nrow(realts_TRAIN), ]
+realts_TRAIN <- realts_TRAIN[-nrow(realts_TRAIN), ]
+
+classes <- c(classes_TEST, classes_TRAIN)
+rm(classes_TEST, classes_TRAIN)
+
+
+## store all UTS sets
+realts <- list()
+idx <- 1
+
+# TEST
+for(test in 1:ncol(realts_TEST)) {
+  if(length(which(is.na(realts_TEST[, test]))) > 0)
+    ts_aux <- ts(realts_TEST[1:(which(is.na(realts_TEST[, test]))[1] - 1), test])
+  else
+    ts_aux <- ts(realts_TEST[1:(nrow(realts_TEST)), test])
+  realts[[idx]] <- ts_aux
+  idx <- idx+1
+}
+# TRAIN
+for(train in 1:ncol(realts_TRAIN)) {
+  if(length(which(is.na(realts_TRAIN[, train]))) > 0)
+    ts_aux <- ts(realts_TRAIN[1:(which(is.na(realts_TRAIN[, train]))[1] - 1), train])
+  else
+    ts_aux <- ts(realts_TRAIN[1:(nrow(realts_TRAIN)), train])
+  realts[[idx]] <- ts_aux
+  idx <- idx+1
+}
+
+rm(ts_aux, idx, test, train, realts_TEST, realts_TRAIN)
