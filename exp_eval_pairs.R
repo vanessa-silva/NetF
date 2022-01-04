1588166aaba378479e9ad9c3b2cdf1be04d41fa9
Vanessa Silva <vanessaaf_silva@hotmail.com>
2022-01-04 13:55:57 +0000

    adding main procedure to run experimental evaluation of benchmark empirical dataset (the 18Pairs)

---
+++ exp_eval_pairs.R
@@ -0,0 +1,131 @@
+#############################################################
+################## Experimental Evaluation ##################
+#############################################################
+
+
+### auxiliary variables to store results
+dataset <- "realts"
+data_name <- "pairs"
+
+total_comb <- 3
+
+table_eval_mean <- data.frame("ARI" = array(0, total_comb),
+                              "NMI" = array(0, total_comb),
+                              "AS" = array(0, total_comb),
+                              "k" = array(0, total_comb))
+table_eval_mean_2 <- data.frame("ARI" = array(0, total_comb),
+                                "NMI" = array(0, total_comb),
+                                "AS" = array(0, total_comb),
+                                "k" = array(0, total_comb))
+
+table_eval_sd <- data.frame("ARI" = array(0, total_comb),
+                            "NMI" = array(0, total_comb),
+                            "AS" = array(0, total_comb),
+                            "k" = array(0, total_comb))
+table_eval_sd_2 <- data.frame("ARI" = array(0, total_comb),
+                              "NMI" = array(0, total_comb),
+                              "AS" = array(0, total_comb),
+                              "k" = array(0, total_comb))
+
+rownames(table_eval_mean) <- c("NetF", "tsfeature", "catch22")
+rownames(table_eval_mean_2) <- rownames(table_eval_mean)
+rownames(table_eval_sd) <- rownames(table_eval_mean)
+rownames(table_eval_sd_2) <- rownames(table_eval_mean)
+
+## create a seed to the clustering task
+set.seed(2020)
+
+
+#######################################################
+#### clustering analysis -- NetF
+
+source("func_clustering.R")
+
+## load features
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Metrics/realts/pairs_normetrics_wnvg_realts.RData"))
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Metrics/realts/pairs_normetrics_whvg_realts.RData"))
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Metrics/realts/pairs_normetrics_50qg_Mkv_realts.RData"))
+
+## auxiliary variables
+normetrics <- cbind(nm_wnvg_realts, 
+                    nm_whvg_realts, 
+                    nm_qg_realts, classes)
+colnames(normetrics) <- c("k_WNVG", "d_WNVG", "S_WNVG", "C_WNVG", "Q_WNVG",
+                          "k_WHVG", "d_WHVG", "S_WHVG", "C_WHVG", "Q_WHVG",
+                          "k_50-QG", "d_50-QG", "S_50-QG", "C_50-QG", "Q_50-QG",
+                          "Class")
+
+nvg <- 1:5
+hvg <- 6:10
+q50 <- 11:15
+k_1 <- length(unique(normetrics[, ncol(normetrics)]))
+k_2 <- k_1
+
+indx_t <- 1
+
+i <- 1
+
+title <- "NetF"
+
+## experimental analysis of network features
+### WNVG - WHVG - 50-QG
+mappings <- "NetF"
+idxs <- c(nvg, hvg, q50)
+source("comp_clustering.R")
+pcaplots$pca_plot
+
+
+#######################################################
+#### clustering analysis -- tsfeatures
+
+## load features
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Metrics/realts/pairs_normetrics_tsfeature_realts.RData"))
+
+## auxiliary variables
+nmetrics_Hynd <- cbind(nmetrics_Hynd, classes)
+
+k_1 <- length(unique(nmetrics_Hynd$classes))
+k_2 <- k_1
+
+title <- "tsfeature"
+
+## experimental analysis of Hyndman features from 'tsfeature' package
+mappings <- "tsfeature"
+normetrics <- nmetrics_Hynd
+idxs <- 1:ncol(normetrics)-1
+source("comp_clustering.R")
+pcaplots$pca_plot
+
+
+#######################################################
+#### clustering analysis -- Rcatch22
+
+## load features
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Metrics/realts/pairs_normetrics_catch22_realts.RData"))
+
+## auxiliary variables
+nmetrics_catch22 <- cbind(nmetrics_catch22, classes)
+
+k_1 <- length(unique(nmetrics_catch22$classes))
+k_2 <- k_1
+
+title <- "Rcatch22"
+
+## experimental analysis of catch22 features from 'Rcatch22' package
+mappings <- "Rcatch22"
+normetrics <- nmetrics_catch22
+idxs <- 1:ncol(normetrics)-1
+source("comp_clustering.R")
+pcaplots$pca_plot
+
+
+#######################################################
+#### View results
+
+## for ground truth
+table_eval_mean
+table_eval_sd
+
+## for best k
+table_eval_mean_2
+table_eval_sd_2
---
+++ main_features_pairs.R
@@ -0,0 +1,104 @@
+#############################################################
+##################### GENERATE GRAPHS #######################
+#############################################################
+
+## load datasets
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Data/real_ts.RData"))
+
+#######################################################
+#### Generate Graphs using the Time Series Mappings
+
+## (W)NVG
+wnvg_realts <- generate_Graphs_list(pairs_ts, length(pairs_ts), map_type = "NVG", weight_type = TRUE)
+
+## (W)HVG
+whvg_realts <- generate_Graphs_list(pairs_ts, length(pairs_ts), map_type = "HVG", weight_type = TRUE)
+
+## QG Markov
+qg50_realts <- generate_Graphs_list(pairs_ts, length(pairs_ts), q = 50, map_type = "QG", is_Markov = TRUE)
+
+
+#############################################################
+################ CALCULATE FEATURES - NetF ##################
+#############################################################
+
+#######################################################
+#### calculate NetF - Network Features
+
+## (W)NVG
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Graphs/realts/pairs_wnvg_realts.RData"))
+
+m_wnvg_realts <- calc_metrics(wnvg_realts, length(wnvg_realts), map_type = "NVG", weight_type = TRUE)
+
+## (W)HVG
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Graphs/realts/pairs_whvg_realts.RData"))
+
+m_whvg_realts <- calc_metrics(whvg_realts, length(whvg_realts), map_type = "HVG", weight_type = TRUE)
+
+## QG Markov
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Graphs/realts/pairs_50qg_Mkv_realts.RData"))
+
+m_qg_realts <- calc_metrics(qg50_realts, length(qg50_realts), map_type = "QG")
+
+
+#######################################################
+#### normalize NetF
+
+## load auxiliary function
+source("min_max_norm.R")
+
+## (W)NVG
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Metrics/realts/pairs_metrics_wnvg_realts.RData"))
+
+summary(m_wnvg_realts)
+nm_wnvg_realts <- norm_data(m_wnvg_realts)
+
+## (W)HVG
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Metrics/realts/pairs_metrics_whvg_realts.RData"))
+
+summary(m_whvg_realts)
+nm_whvg_realts <- norm_data(m_whvg_realts)
+
+## QG Mkv
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Metrics/realts/pairs_metrics_50qg_Mkv_realts.RData"))
+
+summary(m_qg_realts)
+nm_qg_realts <- norm_data(m_qg_realts)
+
+
+#############################################################
+########## CALCULATE FEATURES - Classical Features ##########
+#############################################################
+
+#######################################################
+#### calculate features - 'tsfeature' package
+
+metrics_Hynd <- tsfeatures(pairs_ts)
+summary(metrics_Hynd)
+
+#######################################################
+#### normalize tsfeature
+
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Metrics/realts/pairs_metrics_tsfeature_realts.RData"))
+
+nmetrics_Hynd <- norm_data(metrics_Hynd)
+
+
+#######################################################
+#### calculate features - 'Rcatch22' package
+
+aux <- catch22_all(pairs_ts[[1]])
+metrics_catch22 <- as.data.frame(t(aux$values))
+colnames(metrics_catch22) <- aux$names
+for(i in 2:length(pairs_ts)) {
+  aux <- catch22_all(pairs_ts[[i]])
+  metrics_catch22 <- rbind(metrics_catch22, c(t(aux$values)))
+}
+summary(metrics_catch22)
+
+#######################################################
+#### normalize catch22
+
+load(url("https://www.dcc.fc.up.pt/~vanessa.silva/datasets/NetF/Metrics/realts/pairs_metrics_catch22_realts.RData"))
+
+nmetrics_catch22 <- norm_data(metrics_catch22)
